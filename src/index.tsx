bbd210c0e21f1fa0f815601b278d1700f7bfd7d4
zhangwenbo.alobe <zhangwenbo.alobe@bytedance.com>
2022-05-16 15:43:30 +0800

    style: adjust
    Change-Id: Idc599ea09bdd0131462ac787c835f0e5fab995db

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,71 @@
-import * as React from 'react'
-import { getReactFibers, getDisplayName, getDetail } from './utils/react-analysis'
-export * from './utils/react-analysis'
-export * from './constant'
+import * as React from 'react';
+import { getReactFibers, getDisplayName, getDetail } from './utils/react-analysis';
+export * from './utils/react-analysis';
+export * from './constant';
+
+const { useState, useEffect, useRef } = React;
 
 export const Analysis = () => {
-  const [target, setTarget] = React.useState<HTMLElement | null>(null)
+  const [target, setTarget] = useState<HTMLElement | null>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const cb = (e: MouseEvent) => {
       e.preventDefault();
-      if (e.target instanceof HTMLElement) setTarget(e.target)
-    }
-    document.addEventListener('contextmenu', cb)
-    return () => document.removeEventListener('contextmenu', cb)
-  }, [])
+      if (!(e.target instanceof HTMLElement)) return;
+      if (ref.current?.contains(e.target as HTMLElement)) return;
+      setTarget(e.target);
+    };
+    document.addEventListener('contextmenu', cb);
+    return () => document.removeEventListener('contextmenu', cb);
+  }, []);
 
-  const fibers = getReactFibers(target).filter(f => f._debugSource)
-  const position = target?.getBoundingClientRect()
+  useEffect(() => {
+    const cb = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as HTMLElement)) setTarget(null);
+    };
+    document.addEventListener('click', cb);
+    return () => document.removeEventListener('click', cb);
+  }, []);
+
+  const fibers = getReactFibers(target).filter(f => f._debugSource);
   return target && fibers.length ? (
-    <div onContextMenu={e => (e.stopPropagation(), e.preventDefault())} style={{top: position!.top, left: position!.left, zIndex: 99999, border: '1px solid gray', position: 'fixed', borderRadius: 4, padding: 10, display: 'flex', backgroundColor: 'white', flexDirection: 'column'}}>
-      <div className=""><span style={{cursor: 'default'}} onClick={() => setTarget(null)}>Close</span></div>
+    <div
+      ref={ref}
+      onContextMenu={e => (e.stopPropagation(), e.preventDefault())}
+      style={{
+        bottom: 20,
+        right: 20,
+        zIndex: 99999,
+        border: '1px solid rgba(153, 51, 255)',
+        boxShadow: '3px 3px 5px rgba(153, 51, 255, 0.5)',
+        position: 'fixed',
+        borderRadius: 4,
+        padding: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white'
+      }}>
       {fibers.map(f => {
-        const detail = getDetail(f)
+        const detail = getDetail(f);
         return (
-          <div style={{cursor: 'pointer', marginBottom: 4}} key={f._debugID} onClick={(e) => (e.preventDefault(), window.open(detail.url))}>
-            <div style={{marginRight: 6, color: 'rgba(51, 112, 255, 1)', fontSize: 16, fontWeight: 500}}>{getDisplayName(f)}: </div>
-            <div className="">{detail.fileName.split('src/')?.[1]}:{detail.lineNumber}</div>
+          <div
+            style={{
+              cursor: 'pointer',
+              borderRadius: 4,
+              padding: '0 4px',
+            }}
+            key={f._debugID}
+            // eslint-disable-next-line no-console
+            onMouseOver={() => console.info('Component pendingProps =>>>> ', f.pendingProps)}
+            onClick={e => (e.preventDefault(), window.open(detail.url))}>
+            <div style={{color: 'rgba(153, 51, 255)', fontWeight: 500, fontSize: 16}}>{getDisplayName(f)}: </div>
+            <div style={{fontSize: 12, color: '#8f959e'}}>
+              {detail.fileName.split('src/')?.[1]}:{detail.lineNumber}
+            </div>
           </div>
-        )
+        );
       })}
     </div>
-  ) : null
-}
+  ) : null;
+};
